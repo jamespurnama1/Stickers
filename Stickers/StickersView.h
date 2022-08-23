3ee6018fe32af63eb5fba9978236841523bf1030
jamespurnama1 <james.henry.purnama0@gmail.com>
2022-08-23 21:28:13 +0700

    Initial Commit

---
+++ Stickers/StickersView.h
@@ -0,0 +1,12 @@
+//
+//  StickersView.h
+//  Stickers
+//
+//  Created by James Henry on 23/08/22.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface StickersView : ScreenSaverView
+
+@end
---
+++ Stickers/StickersView.m
@@ -0,0 +1,51 @@
+//
+//  StickersView.m
+//  Stickers
+//
+//  Created by James Henry on 23/08/22.
+//
+
+#import "StickersView.h"
+
+@implementation StickersView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
